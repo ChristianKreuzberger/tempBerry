9fca76a40eb6618dcffe7d68dbfcbff588a9d6a2
Christian Kreuzberger <ckreuzberger@anexia-it.com>
2017-01-18 20:58:29 +0100

    Made URLs routing static and added last update field to UI

--- tempBerry/temperaturesFrontend/urls.py
+++ tempBerry/temperaturesFrontend/urls.py
@@ -1,7 +1,20 @@
 from django.conf.urls import url, include
 from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.views import serve
 
 # Redirect any request that goes into here to static/index.html
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='static/', permanent=False), name='index')
+    # url(r'^$', RedirectView.as_view(url='static/', permanent=False), name='index')
+    # url(r'^$', TemplateView.as_view(template_name='index.html'))
+
+    # serve index.htmla nd static things directly
+    # taken from http://stackoverflow.com/a/40525517
+    # and http://stackoverflow.com/questions/27065510/how-to-serve-static-files-with-django-that-has-hardcoded-relative-paths-on-herok/40525157#40525157
+
+    url(r'^$', serve,
+        kwargs={'path': 'index.html'}),
+
+    # static files (*.css, *.js, *.jpg etc.) served on /
+    url(r'^(?!/static/.*)(?P<path>.*\..*)$',
+        RedirectView.as_view(url='/static/%(path)s')),
 ]
