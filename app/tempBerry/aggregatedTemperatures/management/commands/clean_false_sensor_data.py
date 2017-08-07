5a09b61912194e8aab5813feddf5ae970154d6ba
Christian Kreuzberger <s4ko88@gmail.com>
2017-08-07 20:15:23 +0200

    More cleanup commands

--- app/tempBerry/aggregatedTemperatures/management/commands/clean_false_sensor_data.py
+++ app/tempBerry/aggregatedTemperatures/management/commands/clean_false_sensor_data.py
@@ -9,8 +9,13 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
 
-    def handle(self, *args, **options):
-        # get all sensor ids
+    def delete_data_with_less_than(self, entries=5):
+        """
+        Delete all temperature data entries where for a given sensor id there are less than $entries entries
+        :param entries:
+        :return:
+        """
+        # get all sensor ids and the total count of entries
         sensor_ids = TemperatureDataEntry.objects.values('sensor_id').annotate(
             total=Count('sensor_id')
         ).order_by('total')
@@ -21,13 +26,37 @@ class Command(BaseCommand):
                 # check if public room exists
                 rooms = Room.objects.filter(sensor_id=sensor_stats['sensor_id'])
                 if not rooms.exists():
-                    # no rooms eixsts, we can safely delete this
+                    # no rooms exists, we can safely delete this
                     do_delete = True
                 elif not rooms.filter(public=True).exists():
-                    # rooms eixsts, we need to delete the rooms but and also the data
+                    # rooms exists, we need to delete the rooms in addition to the data
                     rooms.delete()
                     do_delete = True
 
             if do_delete:
                 print("deleting entries for sensor_id=", sensor_stats['sensor_id'])
                 TemperatureDataEntry.objects.filter(sensor_id=sensor_stats['sensor_id']).delete()
+
+    def delete_negative_humidities(self):
+        """
+        Deletes all entries with humidities of less than 0%
+        :return:
+        """
+        entries = TemperatureDataEntry.objects.filter(humidity__lte=0.0)
+        print("Deleting ", entries.count(), " entries with a humidity of 0.0 or less")
+        entries.delete()
+
+    def delete_very_negative_temperatures(self, threshold=-30):
+        """
+        Deletes all entries with temperatures of less than threshold °C (e.g., -30)
+        :param threshold:
+        :return:
+        """
+        entries = TemperatureDataEntry.objects.filter(temperature__lte=threshold)
+        print("Deleting ", entries.count(), " entries with a temperature of ", threshold, "°C or less")
+        entries.delete()
+
+    def handle(self, *args, **options):
+        self.delete_data_with_less_than(entries=5)
+        self.delete_negative_humidities()
+        self.delete_very_negative_temperatures()
