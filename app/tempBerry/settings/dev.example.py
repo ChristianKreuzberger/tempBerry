bae0ca97945ab6138bc1dc40c91610118525e1c7
Christian Kreuzberger <s4ko88@gmail.com>
2018-08-23 18:36:18 +0200

    Make sure mysql has a separate volume and isnt started as root; add a better example configuration

---
+++ app/tempBerry/settings/dev.example.py
@@ -0,0 +1,22 @@
+from tempBerry.settings.base import *
+
+DEBUG = True
+
+# PLEASE ENTER A SECRET KEY HERE
+SECRET_KEY = ""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tempberry',
+        'USER': 'tempberry',
+        'PASSWORD': 'tempberry',
+        'HOST': '127.0.10.1',
+        'PORT': '3306'
+    }
+}
+
+# allow all hosts for now
+ALLOWED_HOSTS = [
+    "*"
+]
