ce68a2c906db567f6db34bde67e3275444b14fb6
Christian Kreuzberger <s4ko88@gmail.com>
2020-01-03 11:41:36 +0100

    Initial setup for whitenoise

--- app/tempBerry/settings/live.py
+++ app/tempBerry/settings/live.py
@@ -2,3 +2,6 @@ from tempBerry.settings.base import *
 
 # ensure that debug is false
 DEBUG = False
+
+# ensure whitenoise static file storage is used
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
